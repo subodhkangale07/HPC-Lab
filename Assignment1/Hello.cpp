2cb8635c7a82f2036ad749363b2af8514bf7cba9
Subodh kangale <125494916+subodhkangale07@users.noreply.github.com>
2025-08-05 09:37:19 +0530

    push other

--- Assignment1/Hello.cpp
+++ Assignment1/Hello.cpp
@@ -3,12 +3,7 @@ using namespace std;
 int main() {
     #pragma omp parallel
     {
-
-<<<<<<< HEAD
         cout << "Hello Subodh\n";
-=======
-        cout << "Hello world\n";
->>>>>>> 09c868ef755cfc027e50a75e5bf29de11ee5777b
 
     }
 }   
